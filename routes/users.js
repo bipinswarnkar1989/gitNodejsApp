33164c5f294237c1a4be780307b831d2cc0d15d9
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2019-04-05 10:46:47 +0530

    Added user repo issues api

--- routes/users.js
+++ routes/users.js
@@ -21,7 +21,7 @@ router.get('/auth', function(req, res, next) {
   // })
 });
 
-/* GET users listing. */
+/* GET user data. */
 router.get('/:username', function(req, res, next) {
   const username = req.params.username;
   fetch('https://api.github.com/users/'+username)
@@ -32,10 +32,24 @@ router.get('/:username', function(req, res, next) {
     });
 });
 
-/* GET users listing. */
+
+/* GET user repos. */
 router.get('/:username/repos', function(req, res, next) {
   const username = req.params.username;
   fetch('https://api.github.com/users/'+username+'/repos')
+    .then(res => res.json())
+    .then(json => {
+      console.log(json);
+      return res.json(json)
+    });
+});
+
+/* GET user repo issues. */
+router.get('/:username/:repo/issues', function(req, res, next) {
+  const username = req.params.username;
+  const repo = req.params.repo;
+  //https://api.github.com/repos/bipinswarnkar1989/prepare-online-Mern-App/issues
+  fetch('https://api.github.com/repos/'+username+'/'+repo+'/issues')
     .then(res => res.json())
     .then(json => {
       console.log(json);
